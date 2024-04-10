24de6b5dd2cb99fc3117dfc1204a8d7503a9980e
aitoAarni <aarni.rechardt@gmail.com>
2024-04-10 07:51:10 +0300

    new e2e test added

--- e2e-tests/testing123.spec.js
+++ e2e-tests/testing123.spec.js
@@ -10,4 +10,18 @@ describe('Pokedex', () => {
       ),
     ).toBeVisible()
   })
+  test('clicking a pokemon redirects to its page', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+    // Find the link to click
+    // Find the link to click
+    const pokemonLink = await page.$('a[href="/pokemon/ivysaur"]')
+    if (pokemonLink) {
+      // Click the link
+      await pokemonLink.click()
+    } else {
+      throw new Error('Pokemon link not found')
+    }
+
+    expect(page.getByText('chlorophyll'))
+  })
 })
